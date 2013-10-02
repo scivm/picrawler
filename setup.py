f8c51a73e5e7c9d9a92530bf2281a7cc649d0619
Ikuya Yamada <ikuya@ikuya.net>
2013-10-02 16:44:27 +0900

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='picrawler',
-    version='0.0.5',
+    version='0.0.6',
     description='A distributed web crawler using PiCloud.',
     author='Studio Ousia',
     author_email='admin@ousia.jp',
